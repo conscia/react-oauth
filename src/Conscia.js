76a00742730f87a65310970c753c605bceed227d
ferron <ferronrsmith@gmail.com>
2016-12-14 23:51:43 -0400

    minor tweaks

--- src/Conscia.js
+++ src/Conscia.js
@@ -1,21 +1,13 @@
 import React, { PropTypes } from 'react';
 import OAuth2               from './OAuth2';
 
-export const styles = {
-  button: {
-    backgroundColor: 'steelblue'
-  }
-};
-
 const Conscia = props => {
-  const { config, textDisplay, style, successCallback, errorCallback } = props;
-  let baseStyles = [styles.button];
-  if (style) baseStyles.push(style);
+  const { config, textDisplay, className, successCallback, errorCallback } = props;
   config.successCallback = successCallback;
   config.errorCallback = errorCallback;
   return (
     <OAuth2 {...config}>
-      <button style={baseStyles}>{textDisplay}</button>
+      <button className={className}>{textDisplay}</button>
     </OAuth2>
   );
 };
@@ -27,7 +19,7 @@ Conscia.defaultProps = {
 Conscia.propTypes = {
   config     : PropTypes.object.isRequired,
   textDisplay: PropTypes.string,
-  style      : PropTypes.object
+  className      : PropTypes.string
 };
 
 export default Conscia;
